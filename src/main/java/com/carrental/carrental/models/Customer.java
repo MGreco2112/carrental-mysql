0a5a8991c059f0ad1a96a34ace2dd79cd76bbff6
Matthew Greco <mgreco2112@tutanota.com>
2021-11-05 17:15:59 -0400

    created Vehicle and Customer classes

--- src/main/java/com/carrental/carrental/models/Customer.java
+++ src/main/java/com/carrental/carrental/models/Customer.java
@@ -6,6 +6,50 @@ import javax.persistence.Id;
 
 @Entity
 public class Customer {
-    @Id @GeneratedValue Long id;
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private Boolean isMember;
+    private String licenseNumber;
 
+    public Customer() {
+
+    }
+
+    public Customer(String name, Boolean isMember, String licenseNumber) {
+        this.name = name;
+        this.isMember = isMember;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getMember() {
+        return isMember;
+    }
+
+    public void setMember(Boolean member) {
+        isMember = member;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
 }
--- src/main/java/com/carrental/carrental/models/Vehicle.java
+++ src/main/java/com/carrental/carrental/models/Vehicle.java
@@ -6,7 +6,60 @@ import javax.persistence.Id;
 
 @Entity
 public class Vehicle {
-    @Id @GeneratedValue Long id;
+    private @Id @GeneratedValue Long id;
+    private String make;
+    private String model;
+    private String year;
+    private Integer miles;
 
+    public Vehicle() {
 
+    }
+
+    public Vehicle(String make, String model, String year, Integer miles) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.miles = miles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Integer getMiles() {
+        return miles;
+    }
+
+    public void setMiles(Integer miles) {
+        this.miles = miles;
+    }
 }

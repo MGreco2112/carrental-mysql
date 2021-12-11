7ecf8f91d6664e5383b545c58bd23c2ae7a65875
Matthew Greco <mgreco2112@tutanota.com>
2021-12-11 15:36:29 -0500

    added more security classes

--- src/main/java/com/carrental/carrental/security/WebSecurityConfig.java
+++ src/main/java/com/carrental/carrental/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.carrental.carrental.security;
 
 import com.carrental.carrental.security.jwt.AuthEntryPointJwt;
+import com.carrental.carrental.security.jwt.AuthTokenFilter;
 import com.carrental.carrental.security.services.UserDetailsImpl;
 import com.carrental.carrental.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -34,7 +36,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
